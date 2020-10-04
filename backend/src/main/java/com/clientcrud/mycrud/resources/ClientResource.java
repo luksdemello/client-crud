ba9bbddc72153e50fb1dd182732cfe7b5bc2b2ad
Lucas de Mello <lucas_mello6@hotmail.com>
2020-10-04 16:59:07 -0300

    DTO

--- backend/src/main/java/com/clientcrud/mycrud/resources/ClientResource.java
+++ backend/src/main/java/com/clientcrud/mycrud/resources/ClientResource.java
@@ -1,14 +1,16 @@
 package com.clientcrud.mycrud.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.clientcrud.mycrud.entities.Client;
+import com.clientcrud.mycrud.dto.ClientDTO;
 import com.clientcrud.mycrud.services.ClientService;
 
 @RestController
@@ -19,8 +21,16 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = service.findAll();
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,	
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy	
+			){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<ClientDTO> list = service.findAll(pageRequest);
 		
 		
 		return ResponseEntity.ok(list);
--- backend/src/main/java/com/clientcrud/mycrud/services/ClientService.java
+++ backend/src/main/java/com/clientcrud/mycrud/services/ClientService.java
@@ -1,11 +1,12 @@
 package com.clientcrud.mycrud.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.clientcrud.mycrud.dto.ClientDTO;
 import com.clientcrud.mycrud.entities.Client;
 import com.clientcrud.mycrud.repositories.ClientRepository;
 
@@ -18,7 +19,10 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public Page<ClientDTO> findAll(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		
+		return list.map(x -> new ClientDTO(x));
+		
 	}
 }
