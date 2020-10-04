4923d27d3cf42f54cf54e74087fba3ef74a51fa5
Lucas de Mello <lucas_mello6@hotmail.com>
2020-10-04 18:14:25 -0300

    Update client

--- backend/src/main/java/com/clientcrud/mycrud/services/ClientService.java
+++ backend/src/main/java/com/clientcrud/mycrud/services/ClientService.java
@@ -13,31 +13,31 @@ import org.springframework.transaction.annotation.Transactional;
 import com.clientcrud.mycrud.dto.ClientDTO;
 import com.clientcrud.mycrud.entities.Client;
 import com.clientcrud.mycrud.repositories.ClientRepository;
-
-
+import com.clientcrud.mycrud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
-		
+
 		return list.map(x -> new ClientDTO(x));
-		
+
 	}
 
 	@Transactional(readOnly = true)
 	public ClientDTO findByID(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found!"));
-		
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
+
 		return new ClientDTO(entity);
 	}
 
+	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
 		copyDtoToEntity(dto, entity);
@@ -45,12 +45,27 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
-		entity.setBithDate(dto.getBirthDate());;
+		entity.setBithDate(dto.getBirthDate());
+		;
 		entity.setChildren(dto.getChildren());
-		
+
 	}
+
 }
